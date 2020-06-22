5547b6e36d7c65814fc1574f7f12d5337821efa4
= <yoshikiito.el@gmail.com>
2020-06-22 23:52:05 +0900

    Update sample1.py

--- sample1.py
+++ sample1.py
@@ -5,7 +5,7 @@ from faker.providers import profile
 
 fake = Faker('ja_JP')
 fake.add_provider(profile)
-with open('./fake_data.csv', 'w') as f:
+with open('./fake_data.csv','w') as f:
     writer = csv.writer(f)
     for _ in range(100):
-        writer.writerow([fake.name(), random.randint(18,75), "男性" if fake.simple_profile()['sex'] == 'M' else "女性", fake.address(), fake.job()])
+        writer.writerow([fake.name(), random.randint(18, 75), "男性" if fake.simple_profile()['sex'] == 'M' else "女性", fake.address(), fake.job()])
